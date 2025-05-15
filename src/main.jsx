0dc29df28967a55768f74141e964fbcb5235ef18
krrish0508 <krishnarajm08@gmail.com>
2025-05-15 18:11:22 -0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import Portfolio from "./App.jsx";
+import "./index.css";
 
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Portfolio from './Portfolio'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Portfolio />
+    <BrowserRouter basename="/krishnaraj-portfolio">
+      <Portfolio />
+    </BrowserRouter>
   </React.StrictMode>
-)
+);
