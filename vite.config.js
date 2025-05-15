b07d5e07a753a6932aff6b886eed64f53ebe7626
krrish0508 <krishnarajm08@gmail.com>
2025-05-15 16:45:14 -0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/krishnaraj-portfolio/', // 👈 change this to your repo name
+  base: '/krishnaraj-portfolio/',
   plugins: [react()],
 })
