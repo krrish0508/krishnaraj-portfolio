b274d0b60d070a200993a67e32675b165f7f4734
krrish0508 <krishnarajm08@gmail.com>
2025-05-15 17:59:37 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/krishnaraj-portfolio/',
+  base: '/krishnaraj-portfolio/', // IMPORTANT: match your repo name
   plugins: [react()],
-})
+});
